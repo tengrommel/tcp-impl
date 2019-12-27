82c9adcbcc7fca83e1a3df7dc26fd4cab4aa7245
红雀 <wwwsousoucom@126.com>
2019-12-27 03:16:00 -0800

    first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+extern crate tun_tap;
+use std::io;
+
+fn main() -> io::Result<()>{
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buf = [0u8; 1504];
+    // receive ip packet
+    let nbytes = nic.recv(&mut buf[..])?;
+    println!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+    Ok(())
 }
